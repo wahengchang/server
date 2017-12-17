51f58706743d85d4a2bc04ca031e17baa3225818
waheng <peterchang@yahoo-inc.com>
2017-12-17 22:39:11 +0800

    Todo for everyone

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ const port = process.env.PORT || 3000
 
 var app = express()
 
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+// Goal mbr sysem
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*/
+
+// GET  /api/v1/users/login
+// GET  /api/v1/users/logout
+// GET  /api/v1/users/signup
+
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+// Ronald Todo
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*/
+//  Knowledge
+//      Unit test
+//      Promise
+//  Implement
+//      monogoDB - user
+//
+// app.use('/api/v1/users', require('./router/users'))
+
+
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+// Sam Todo
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*/
+//  Knowledge
+//      MongoLab
+//      bike (CURD: Create, Update, Read, Delete)
+
 app.get('/', function (req, res) {
     res.send('hello')
 })
---
+++ routers/users.js
@@ -0,0 +1,2 @@
+// Reference
+// https://github.com/globalbanana/website/blob/dev/src/router/login.js
